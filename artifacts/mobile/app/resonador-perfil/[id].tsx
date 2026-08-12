7ddd30d37cf9cb024af4fc10e9c993c6299ed0f3
ResonanciaApp <contacto@appresonancia.cl>
2026-08-12 03:07:52 +0000

    design: 'Resonador Verificado' en #E5D6FF

--- artifacts/mobile/app/resonador-perfil/[id].tsx
+++ artifacts/mobile/app/resonador-perfil/[id].tsx
@@ -784,7 +784,7 @@ const styles = StyleSheet.create({
     fontFamily: "Manrope",
     fontSize: 12,
     fontWeight: "400",
-    color: "#DDC7FF",
+    color: "#E5D6FF",
     textAlign: "center",
     marginTop: -2,
   },
